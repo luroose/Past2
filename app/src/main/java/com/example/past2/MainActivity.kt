6e162e54de17a1b213a047f11511ae8d809e86f8
luroose <636051200195@mail.rmutk.ac.th>
2022-02-18 23:19:05 +0700

    Initial commit

---
+++ app/src/main/java/com/example/past2/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.example.past2
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        button.setOnClickListener {
+            val i = Intent(this, MainActivity2::class.java)
+            startActivity(i)
+        }
+        button2.setOnClickListener {
+            val i = Intent(this, MainActivity3::class.java)
+            startActivity(i)
+        }
+        button3.setOnClickListener {
+            val i = Intent(this, MainActivity4::class.java)
+            startActivity(i)
+        }
+    }
+}
